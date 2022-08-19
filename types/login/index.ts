e804e9587ed42c6b5032cb0787b3e1f8bb20a277
LukaszAmbrozewski <lukaszambrozewski@wp.pl>
2022-08-19 09:20:16 +0200

    Corrected new types and added new package.

--- types/login/index.ts
+++ types/login/index.ts
@@ -1,11 +1,1 @@
-export interface Login {
-    id?: string;
-    user_id: string;
-    refreshToken: string;
-}
-
-export interface LoginCreated {
-    id: string;
-    token: string;
-    refreshToken: string;
-}
+export * from './login';
--- types/login/login.ts
+++ types/login/login.ts
@@ -1,1 +1,11 @@
-export * from './login';
+export interface Login {
+    id?: string;
+    user_id: string;
+    refreshToken: string;
+}
+
+export interface LoginCreated {
+    id: string;
+    token: string;
+    refreshToken: string;
+}
